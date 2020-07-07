28c7136881740122db9035431bdfeb63d68a17d1
r-tezuka <ryuta.tezuka2@gmail.com>
2020-07-07 22:42:40 +0900

    mod data structure

--- src/components/CubicBezier.vue
+++ src/components/CubicBezier.vue
@@ -6,9 +6,10 @@ div
   p
     input#conditions.cubic.rationalBezier(type='checkbox' v-model='conditions.cubic.rationalBezier')
     label(for='conditions.cubic.rationalBezier') display rationalBezier
-  div(v-for='(vertex, index) in this.vertices.cubic')
-    p point{{index}} x:{{vertex.point.x}} y:{{vertex.point.y}} weight: <input v-model=vertex.point.w>
-    p handle{{index}} x:{{vertex.handle.x}} y:{{vertex.handle.y}} weight: <input v-model="vertex.handle.w"> 
+  div(v-for='(vertex, index) in this.points.cubic.vertices')
+    p point{{index}} x:{{vertex.x}} y:{{vertex.y}} weight: <input v-model=vertex.w>
+  div(v-for='(handle, index) in this.points.cubic.handles')
+    p handle{{index}} x:{{handle.x}} y:{{handle.y}} weight: <input v-model="handle.w"> 
   canvas#canvas(@mousedown='onDown', @mouseup='onUp', @mouseout='onUp', @mousemove='onMove')
 </template>
 
@@ -26,50 +27,54 @@ export default {
           rationalBezier: false
         }
       },
-      vertices: {
-        cubic: [
-          {
-            point: {
+      points: {
+        cubic: {
+          vertices: [
+            {
               x: 150,
               y: 100,
               w: 1
             },
-            handle: {
-              x: 200,
-              y: 50,
-              w: 1
-            }
-          },
-          {
-            point: {
+            {
               x: 350,
               y: 100,
               w: 1
+            }
+          ],
+          handles: [
+            {
+              x: 200,
+              y: 50,
+              w: 1
             },
-            handle: {
+            {
               x: 300,
               y: 50,
               w: 1
             }
-          }
-        ],
-        quadratic: [
-          {
-            x: 550,
-            y: 100,
-            w: 1
-          },
-          {
-            x: 750,
-            y: 100,
-            w: 1
-          },
-          {
-            x: 650,
-            y: 100,
-            w: 1
-          }
-        ]
+          ]
+        },
+        quadratic: {
+          vertices: [
+            {
+              x: 550,
+              y: 100,
+              w: 1
+            },
+            {
+              x: 750,
+              y: 100,
+              w: 1
+            }
+          ],
+          handles: [
+            {
+              x: 650,
+              y: 100,
+              w: 1
+            }
+          ]
+        }
       },
       cubicBezierCurve: [],
       cubicRationalBezierCurve: []
@@ -87,15 +92,15 @@ export default {
       this.canvas.height = 800;
       document.body.appendChild(this.canvas);
       this.ctx = this.canvas.getContext('2d');
-      for(let i in this.vertices.cubic) {
+      for(let i in this.points.cubic.vertices) {
         this.ctx.fillStyle = "rgb(0, 0, 0)";
-        this.ctx.fillRect(this.vertices.cubic[i].point.x-5, this.vertices.cubic[i].point.y-5, 10, 10);
+        this.ctx.fillRect(this.points.cubic.vertices[i].x-5, this.points.cubic.vertices[i].y-5, 10, 10);
         this.ctx.fillStyle = "rgb(0, 0, 255)";
-        this.ctx.fillRect(this.vertices.cubic[i].handle.x-5, this.vertices.cubic[i].handle.y-5, 10, 10);
+        this.ctx.fillRect(this.points.cubic.handles[i].x-5, this.points.cubic.handles[i].y-5, 10, 10);
         this.ctx.strokeStyle = "rgb(0, 0, 255)";
         this.ctx.beginPath();
-        this.ctx.moveTo(this.vertices.cubic[i].point.x, this.vertices.cubic[i].point.y);
-        this.ctx.lineTo(this.vertices.cubic[i].handle.x, this.vertices.cubic[i].handle.y)
+        this.ctx.moveTo(this.points.cubic.vertices[i].x, this.points.cubic.vertices[i].y);
+        this.ctx.lineTo(this.points.cubic.handles[i].x, this.points.cubic.handles[i].y)
         this.ctx.stroke();
       }
       if (this.conditions.cubic.bezier) {
@@ -110,15 +115,15 @@ export default {
       this.cubicBezierCurve = [];
       for(let i = 0; i <= 100; i++) {
         const t = 0.01*i;
-        const x0 = this.vertices.cubic[0].point.x;
-        const x1 = this.vertices.cubic[0].handle.x;
-        const x2 = this.vertices.cubic[1].handle.x; 
-        const x3 = this.vertices.cubic[1].point.x;
+        const x0 = this.points.cubic.vertices[0].x;
+        const x1 = this.points.cubic.handles[0].x;
+        const x2 = this.points.cubic.handles[1].x; 
+        const x3 = this.points.cubic.vertices[1].x;
         const xt = (1-t)**3*x0+3*(1-t)**2*t*x1+3*(1-t)*t**2*x2+t**3*x3;
-        const y0 = this.vertices.cubic[0].point.y;
-        const y1 = this.vertices.cubic[0].handle.y;
-        const y2 = this.vertices.cubic[1].handle.y; 
-        const y3 = this.vertices.cubic[1].point.y;
+        const y0 = this.points.cubic.vertices[0].y;
+        const y1 = this.points.cubic.handles[0].y;
+        const y2 = this.points.cubic.handles[1].y; 
+        const y3 = this.points.cubic.vertices[1].y;
         const yt = (1-t)**3*y0+3*(1-t)**2*t*y1+3*(1-t)*t**2*y2+t**3*y3;
         this.cubicBezierCurve.push({ x:xt, y:yt });
       }
@@ -135,21 +140,21 @@ export default {
       this.cubicRationalBezierCurve = [];
       for(let i = 0; i <= 100; i++) {
         const t = 0.01*i;
-        const w0 = this.vertices.cubic[0].point.w;
-        const w1 = this.vertices.cubic[0].handle.w;
-        const w2 = this.vertices.cubic[1].handle.w;
-        const w3 = this.vertices.cubic[1].point.w;
-        const x0 = this.vertices.cubic[0].point.x;
-        const x1 = this.vertices.cubic[0].handle.x;
-        const x2 = this.vertices.cubic[1].handle.x; 
-        const x3 = this.vertices.cubic[1].point.x;
+        const w0 = this.points.cubic.vertices[0].w;
+        const w1 = this.points.cubic.handles[0].w;
+        const w2 = this.points.cubic.handles[1].w;
+        const w3 = this.points.cubic.vertices[1].w;
+        const x0 = this.points.cubic.vertices[0].x;
+        const x1 = this.points.cubic.handles[0].x;
+        const x2 = this.points.cubic.handles[1].x; 
+        const x3 = this.points.cubic.vertices[1].x;
         const xtc = (1-t)**3*x0*w0+3*(1-t)**2*t*x1*w1+3*(1-t)*t**2*x2*w2+t**3*x3*w3;
         const xtp = (1-t)**3*w0+3*(1-t)**2*t*w1+3*(1-t)*t**2*w2+t**3*w3;
         const xt = xtc/xtp;
-        const y0 = this.vertices.cubic[0].point.y;
-        const y1 = this.vertices.cubic[0].handle.y;
-        const y2 = this.vertices.cubic[1].handle.y; 
-        const y3 = this.vertices.cubic[1].point.y;
+        const y0 = this.points.cubic.vertices[0].y;
+        const y1 = this.points.cubic.handles[0].y;
+        const y2 = this.points.cubic.handles[1].y; 
+        const y3 = this.points.cubic.vertices[1].y;
         const ytc = (1-t)**3*y0*w0+3*(1-t)**2*t*y1*w1+3*(1-t)*t**2*y2*w2+t**3*y3*w3;
         const ytp = (1-t)**3*w0+3*(1-t)**2*t*w1+3*(1-t)*t**2*w2+t**3*w3;
         const yt = ytc/ytp;
@@ -170,18 +175,18 @@ export default {
 
       let x = e.clientX - offsetX;
       let y = e.clientY - offsetY;
-      for(var i in this.vertices.cubic) {
-        const point = this.vertices.cubic[i].point;
-        const handle = this.vertices.cubic[i].handle;
+      for(var i in this.points.cubic.vertices) {
+        const point = this.points.cubic.vertices[i];
+        const handle = this.points.cubic.handles[i];
         if (point.x-5 < x && (point.x + 5) > x && point.y-5 < y && (point.y + 5) > y) {
           this.dragging = true;
-          this.target = this.vertices.cubic[i].point
+          this.target = this.points.cubic.vertices[i]
           this.relX = this.target.x - x;
           this.relY = this.target.y - y;
         }
         if (handle.x-5 < x && (handle.x + 5) > x && handle.y-5 < y && (handle.y + 5) > y) {
           this.dragging = true;
-          this.target = this.vertices.cubic[i].handle
+          this.target = this.points.cubic.handles[i]
           this.relX = this.target.x - x;
           this.relY = this.target.y - y;
         }
@@ -207,7 +212,7 @@ export default {
     },
   },
   watch: {
-    vertices: {
+    points: {
       handler: function() {
       this.draw();
       },
