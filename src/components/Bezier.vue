db119c0bfbc69c7ef9a9a3c6a3d5db4c83e0d424
r-tezuka <ryuta.tezuka2@gmail.com>
2020-07-20 12:46:50 +0900

    refactor title

--- src/components/Bezier.vue
+++ src/components/Bezier.vue
@@ -46,7 +46,7 @@ div
         label(for='curvature.curveType.bezier') bezier 
         input#curvature.curveType.rationalBezier(type='radio' v-model='curvature.curveType' value='rationalBezier')
         label(for='curvature.curveType.rationalBezier') rationalBezier       
-    span(class="col-4")  3次有理ベジェ曲線をピッチでグレーディング /grading cubic rational bezier grading by pitch:
+    span(class="col-4")  3次有理ベジェ曲線をピッチでグレーディング /grading cubic rational bezier by pitch:
       div
         label(for='cubic.numOfSize') number of sizes: 
         input(type='number', v-model='cubic.numOfSize',style='width:40px;')
